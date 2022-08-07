dff94d68b70edfb895fb3723edc8b5a657edcc3c
Mweh <fahmianton@gmail.com>
2022-08-07 18:26:46 +0800

    v1.1

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -15,12 +15,30 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var tip = 0.1
+    var split = "2"
+    
     @IBAction func tipChanged(_ sender: UIButton) {
-        zeroPctButton.isSelected = true
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        
+        let buttonTitleWithoutPercent = String(buttonTitle.dropLast())
+        
+        tip = Double(buttonTitleWithoutPercent)!/100
+
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        
+        split = splitNumberLabel.text!
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(split)
     }
     
 
